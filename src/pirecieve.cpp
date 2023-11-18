2a86fa8212c291c6a7cc9c17e304e872cd8af12e
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-11-18 16:03:07 -0500

    added pi send and recieve

---
+++ src/pirecieve.cpp
@@ -0,0 +1,30 @@
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <iostream>
+
+int main() {
+    if (wiringPiSetup() == -1) {
+        std::cerr << "Failed to initialize WiringPi." << std::endl;
+        return 1;
+    }
+
+    const char* device = "/dev/ttyS0";
+    
+    int serialPort = serialOpen(device, 9600);
+    if (serialPort == -1) {
+        std::cerr << "Failed to open serial port." << std::endl;
+        return 1;
+    }
+
+    while (true) {
+        while (serialDataAvail(serialPort) > 0) {
+            char receivedChar = serialGetchar(serialPort);
+            std::cout << receivedChar;
+        }
+        delay(100);
+    }
+
+    serialClose(serialPort);
+
+    return 0;
+}
---
+++ src/pisend.cpp
@@ -0,0 +1,28 @@
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <iostream>
+
+int main() {
+    if (wiringPiSetup() == -1) {
+        std::cerr << "Failed to initialize WiringPi." << std::endl;
+        return 1;
+    }
+    
+    const char* device = "/dev/ttyS0";
+
+    int serialPort = serialOpen(device, 9600);
+    if (serialPort == -1) {
+        std::cerr << "Failed to open serial port." << std::endl;
+        return 1;
+    }
+    
+    const char* sendData = "Hello, RS232!";
+    serialPrintf(serialPort, "%s\n", sendData);
+    while (serialDataAvail(serialPort) > 0) {
+        char receivedChar = serialGetchar(serialPort);
+        std::cout << receivedChar;
+    }
+
+    serialClose(serialPort);
+    return 0;
+}
