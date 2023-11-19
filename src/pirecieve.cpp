85f7f5e43820a156ea17ddb36728be215c450651
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-11-18 20:35:07 -0500

    got rid of useless imports

--- src/pirecieve.cpp
+++ src/pirecieve.cpp
@@ -1,13 +1,7 @@
-#include <wiringPi.h>
 #include <wiringSerial.h>
 #include <iostream>
 
 int main() {
-    if (wiringPiSetup() == -1) {
-        std::cerr << "Failed to initialize WiringPi." << std::endl;
-        return 1;
-    }
-
     const char* device = "/dev/serial0";
     
     int serialPort = serialOpen(device, 9600);
