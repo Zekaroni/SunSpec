4dc9905022515312835ec82cc5904b3a73ec3023
Zekaroni <pokemoniscoolnomatterwhat@gmail.com>
2023-11-26 19:30:15 -0500

    updated python serial and debug

--- src/python/winSerial.py
+++ src/python/winSerial.py
@@ -2,16 +2,12 @@ import serial
 
 def monitor_serial(port, baud_rate):
     ser = serial.Serial(port, baud_rate)
-    try:
-        while True:
-            line = ser.readline().decode('utf-8').strip()
-            print(line, end='')
-    except KeyboardInterrupt:
-        print("Monitoring stopped by the user.")
-    finally:
-        ser.close()
+    while True:
+        line = ser.readline().decode('utf-8').strip()
+        print(line)
 
 if __name__ == "__main__":
     serial_port = 'COM14'
     baud_rate = 9600
     monitor_serial(serial_port, baud_rate)
+s
