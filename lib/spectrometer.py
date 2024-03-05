b3168bbb38479114e3e980c4c8ceac0675a142f4
Zekaroni <ezekiel.hall.04@gmail.com>
2024-03-05 16:50:28 -0500

    added spectrometer class init

---
+++ lib/spectrometer.py
@@ -0,0 +1,22 @@
+from seabreeze.spectrometers import Spectrometer
+import numpy as np
+
+class USB2000P:
+    def __init__(self, pixel_offset: int = 48, integration_time: int = 20000) -> None:
+        self.spec = Spectrometer.from_first_available()
+        self.pixel_offset = pixel_offset
+        self.itegration_time = integration_time
+        self.spec.integration_time_micros(self.itegration_time)
+
+    def getSpectra(self) -> dict:
+        waves = self.spec.wavelengths()[self.pixel_offset:]
+        ampli = self.spec.intensities()[self.pixel_offset:]
+        return {waves[i] : ampli[i] for i in range(len(waves))}
+    
+    def getIntensities(self) -> np.ndarray:
+        return self.spec.intensities()[self.pixel_offset:]
+
+
+spec = USB2000P()
+
+print(spec.getSpectra().keys())
