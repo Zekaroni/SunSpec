8af8218ee55ec1f069439d5b4c8d540cc746d0ce
Zekaroni <ezekiel.hall.04@gmail.com>
2024-03-18 23:44:56 -0400

    created baseplate for mainloop

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-# The main script goes here
+from lib.spectrometer    import USB2000P
+from lib.cameraHandler   import Camera
+from lib.imageProcessing import ImageHandler
+from lib.motorDriver     import Motor
+
+from time import time
+
+def mainLoop():
+    # Init all components
+    spectrometer   = USB2000P()
+    camera         = Camera()
+    imageProcessor = ImageHandler()
+    baseMotor      = Motor()
+    cameraMotor    = Motor()
+    
+    while True:
+        timestamp = time()
+
+        # Take image and run it through processing
+        imagePath = f"./output/full/{timestamp}.jpg"
+        camera.takePicture(imagePath)
+        while True:
+            try:
+                image = open(imagePath)
+                processedImage = imageProcessor(image)
+            except OSError:
+                pass
+        
+
+
+
+if __name__ == "__main__":
+    mainLoop()
