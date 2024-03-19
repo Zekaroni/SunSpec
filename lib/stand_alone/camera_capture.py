a953a9a53efe71e0da7a5e867218407efe1fe4a9
Zekaroni <ezekiel.hall.04@gmail.com>
2024-03-18 23:16:45 -0400

    update camera tests

--- lib/stand_alone/camera_capture.py
+++ lib/stand_alone/camera_capture.py
@@ -1,20 +1,12 @@
-import time
-
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
-preview_config = picam2.create_preview_configuration()
-capture_config = picam2.create_still_configuration()
+config = picam2.create_still_configuration()
+picam2.configure(config)
 
-picam2.configure(preview_config)
 picam2.start()
-time.sleep(2)
 
-image = picam2.switch_mode_and_capture_image(capture_config)
-image.show()
-
-
-time.sleep(5)
-
-picam2.close()
+np_array = picam2.capture_array()
+print(np_array)
+picam2.capture_file("demo.jpg")
+picam2.stop()
